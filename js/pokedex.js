a39aeaeb64ba3f41efe649916fad1bc67235a3d6
pitou35 <pierreaugustin.totaro03@gmail.com>
2016-02-24 12:38:48 +0100

    listing et filtrage en fonction de l'input OK

--- js/pokedex.js
+++ js/pokedex.js
@@ -8,4 +8,17 @@ var pokeApiUrl = "http://pokeapi.co/"
 
 pokeApp.controller('MyCtrl', function($scope) {
     //$scope.name = 'Kaliop';
+	//$scope.items = ['sfovjnp', 'sdfgsdf', 'fooqsdfbqsdfbar'];
 });
+
+pokeApp.controller("CtrlList", function($scope) {
+  $scope.poki = [
+    { name: "Peter",   age: 20 },
+    { name: "Pablo",   age: 55 },
+    { name: "Linda",   age: 20 },
+    { name: "Marta",   age: 37 },
+    { name: "Othello", age: 20 },
+    { name: "Markus",  age: 32 }
+  ];
+});
+
